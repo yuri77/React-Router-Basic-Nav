6907464054f651d9e52841c4f3dbef93b71588c7
yurika sakae <yurika.sun@gmail.com>
2019-07-22 13:52:01 -0700

    navigation link created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import { Home, About, Contact, Navigation } from "./components";
 
 const App = () => (
   <div>
     <Navigation />
+    <Switch>
+      <Route exact path="/" t component={Home} />
+      <Route path="/About" component={About} />
+      <Route path="/Contact" component={Contact} />
+    </Switch>
   </div>
 );
 
